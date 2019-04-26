b16d7ce9a289f9318ac0288b3ce603324cb8156d
bkinnel <brennan.kinnel@gmail.com>
2019-04-26 16:20:32 -0400

    Deployment Changes

--- config/prod.exs
+++ config/prod.exs
@@ -10,9 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :learning_goals, LearningGoalsWeb.Endpoint,
-  http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  server: true,
+  root: ".",
+  version: Application.spec(:phoenix_distillery, :vsn),
+  http: [port: {:system, "PORT"}],
+  url: [host: "learn.ozymandiasking.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+
 
 # Do not print debug messages in production
 config :logger, level: :info
