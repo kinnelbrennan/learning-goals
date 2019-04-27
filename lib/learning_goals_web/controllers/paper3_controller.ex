e9304d2cbed7e8e7affa59bb6553afafdd5a7389
Brennan Kinnel <kinnel.b@husky.neu.edu>
2019-04-27 09:26:51 -0400

    final changes

--- lib/learning_goals_web/controllers/paper3_controller.ex
+++ lib/learning_goals_web/controllers/paper3_controller.ex
@@ -2,6 +2,6 @@ defmodule LearningGoalsWeb.Paper3Controller do
   use LearningGoalsWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "unit1.html")
+    render(conn, "unit3.html")
   end
 end
